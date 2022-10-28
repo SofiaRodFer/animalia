3cd113433aa282b7143160dd6a44bc4ea749fd3d
Sofia <sofiarodrigues45@gmail.com>
2022-10-28 18:49:22 -0300

    criando tela inicial

--- App.tsx
+++ App.tsx
@@ -1,21 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'react-native';
+
+import { ThemeProvider } from 'styled-components';
+
+import * as SplashScreen from 'expo-splash-screen';
+
+import theme from './src/styles/theme';
+import {
+  useFonts,
+  VarelaRound_400Regular
+} from '@expo-google-fonts/varela-round'
+
+SplashScreen.preventAutoHideAsync();
+
+const showScreen = async () => await SplashScreen.hideAsync();
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    VarelaRound_400Regular,
+    'Aloja-Light': require('./src/assets/fonts/Aloja-Light.ttf')
+  })
+
+  if (!fontsLoaded) {
+    showScreen()
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={theme}>
+      <StatusBar
+      barStyle='light-content'
+      translucent
+      backgroundColor="transparent"
+      />
+    </ThemeProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/styles/theme.ts
@@ -0,0 +1,15 @@
+export default {
+    colors: {
+        primary: '#F6E7DF',
+        secondary: '#FC7C5C',
+
+        shape: '#FFFFFF',
+        title: '#000000',
+        text: 'rgba(0, 0, 0, 0.7)',
+    },
+
+    fonts: {
+        regular: 'VarelaRound_400Regular',
+        logo: 'Aloja-Light',
+    }
+}
